5a2580b8b8cef7d696d1cb3fc38673f7bf7a54ce
pothuri.saipavan3 <pothuri.saipavan3@gmail.com>
2025-07-26 10:04:50 +0530

    message

---
+++ Day1.sh
@@ -0,0 +1,19 @@
+### check the disk usage and send the mail if the usage is more than 90%
+#!/bin/bash 
+# Get the disk usage percentage
+disk_usage=$(df / | grep / | awk '{ print $5 }' | sed 's/%//g')
+for i in $disk_usage;do
+if [ "$i" -gt 90 ]; then
+  echo "Disk usage is above 90% for  ${disk_usage}%"
+  # Send an email notification (replace with your email command)
+  echo "Disk usage is above 90% on $(hostname): $disk_usage%" | mail -s "Disk Usage Alert" user@example.com
+fi  
+done
+# Check if the disk usage command was successful
+
+# End of script
+echo "Disk usage check completed."
+exit 0
+# This script checks the disk usage and sends an email alert if it exceeds 90%.
+# Make sure to replace 'user@example.com' with the actual email address you want to use.
+

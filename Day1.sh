0f201415333dc5be85a4ccfeeb1095637183040b
pothuri.saipavan3 <pothuri.saipavan3@gmail.com>
2025-07-26 10:09:01 +0530

    message

--- Day1.sh
+++ Day1.sh
@@ -17,3 +17,18 @@ exit 0
 # This script checks the disk usage and sends an email alert if it exceeds 90%.
 # Make sure to replace 'user@example.com' with the actual email address you want to use.
 
+
+#########Print the disk details as well along with the Disk usgae greater than 90 #####
+#Filesystem      Size  Used Avail Use% Mounted on
+#devtmpfs        4.0M     0  4.0M   0% /dev
+#tmpfs           2.0G     0  2.0G   0% /dev/shm
+
+
+
+#!bin/bash
+df -h | awk 'NR>1 {
+  sub(/%/, "", $5);
+  if ($5 > 90)
+    print "High disk usage on " $1 " (" $6 "): " $5 "% used"
+}'
+

53a458d504a0b7269f9dafff8c550631c00eb443
pothuri.saipavan3 <pothuri.saipavan3@gmail.com>
2025-07-27 09:29:39 +0530

    message

--- Day2.sh
+++ Day2.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+### check if the services and processes are running in the server#####
+# Key Differences
+# Services are background programs managed by the system and usually designed to stay running ("daemonized"). You can control and check them easily with commands like systemctl status <service-name>.
+
+# Regular processes are any programs you start from the command line or another program. They exit when finished and aren't automatically restarted or managed by the system.
+
+# In summary:
+# # Managing applications as services provides better control, automated restarts, easier monitoring, and integration with system tools for status checks. Regular processes require manual oversight and intervention if they fail or stop.
+
 
 services=("nginx" "mysql" "my_custom_service")
 process=("python" "node" "myscript")
